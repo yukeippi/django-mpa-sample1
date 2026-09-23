4e7a5e141e578d27b3dce567b480850083afbf76
Yuichiro Kei <yuichirokei@gmail.com>
2026-09-23 09:50:44 +0900

    追加したE2Eテストのimportをファイルの上部に移す

--- app/tests/e2e/task_test.py
+++ app/tests/e2e/task_test.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import Page, expect
+from app.models import Task
 
 
 # タスク一覧ページのE2Eテスト
@@ -124,7 +125,6 @@ class TestTaskStatusModal:
 
     # 詳細画面の「変更」からモーダルでステータスを選んで保存すると、詳細画面のステータス表示が変わること
     def test_change_status_in_modal_updates_detail(self, logged_in_page: Page, live_server_url, e2e_user):
-        from app.models import Task
         task = Task.objects.create(title='Status Task', status='todo', priority=3, created_by=e2e_user)
 
         logged_in_page.goto(f'{live_server_url}/tasks/{task.id}/')
